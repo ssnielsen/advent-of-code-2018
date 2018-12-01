e01b40f99e80c9ad5b362566c236fbf09661a1af
ssnielsen <soeren.nielsen@me.com>
2018-12-01 20:23:22 +0100

    Day 1 changes

--- src/01/day.ts
+++ src/01/day.ts
@@ -1,15 +1,13 @@
 import fs from 'fs';
 import {Set} from 'immutable';
 
-const file = fs.readFileSync('./src/01/input','utf8');
-
-const data = file
+const data = fs
+    .readFileSync('./src/01/input','utf8')
     .split('\n')
     .filter(s => s.length > 0)
     .map(l => {
         const sign = l.substring(0, 1);
-        const rest = l.substring(1);
-        const number = parseInt(rest, 10);
+        const number = parseInt(l.substring(1), 10);
         if (sign === '+' || sign === '-') {
             return {
                 sign,
@@ -36,24 +34,26 @@ export const part1 = () => {
     return data.reduce(change, 0);
 };
 
+function* cycleThrough<T>(array: Array<T>) {
+    let i = 0;
+
+    while (true) {
+        yield array[i % data.length];
+        i++;
+    }
+}
+
 export const part2 = () => {
     let heuristic = Set<number>();
     let frequency = 0;
-    let i = 0;
 
-    while (i < data.length) {
-        frequency = change(frequency, data[i]);
+    for (var instruction of cycleThrough(data)) {
+        frequency = change(frequency, instruction);
 
         if (heuristic.has(frequency)) {
             return frequency;
         } else {
             heuristic = heuristic.add(frequency);
-        }
-
-        if (i === data.length-1) {
-            i = 0;
-        } else {
-            i += 1;
         }
     }
 }
